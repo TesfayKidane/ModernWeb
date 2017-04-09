ca5f197d79874ba2c8bdea0a335be5c79d718f47
Tesfay Aregay <Tesfay Aregay>
2017-04-09 18:37:46 -0500

    json read fixed

--- MyApp/routes/inventors.js
+++ MyApp/routes/inventors.js
@@ -3,11 +3,13 @@ var router = express.Router();
 var path = require('path');
 var fs = require('fs');
 
+var jsonFile = require('./data/inventors.json');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.setHeader('Content-Type', 'application/json');
-  var json = fs.createReadStream(path.join(__dirname,'data','inventors.json'));
-  res.send(json);
+  // res.setHeader('Content-Type', 'application/json');
+  // var json = fs.createReadStream(path.join(__dirname,'data','inventors.json'));
+  // res.send(JSON.parse(json));
+  res.json(jsonFile);
 });
 
 module.exports = router;
